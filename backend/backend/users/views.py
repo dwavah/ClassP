082f886610b864ce8269da6c7901541a28ec6fca
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-04 08:19:59 +0300

    implementing user management api

--- backend/backend/users/views.py
+++ backend/backend/users/views.py
@@ -5,3 +5,7 @@ from .serializers import UserSerializer
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
