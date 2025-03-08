df7e01d099e8d06e5f9e513a76bc2179a1443de8
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-08 14:27:32 +0300

    updates

--- backend/backend/tasks/urls.py
+++ backend/backend/tasks/urls.py
@@ -4,4 +4,6 @@ from .views import TaskListCreateView, TaskDetailView
 urlpatterns = [
     path('', TaskListCreateView.as_view(), name='task-list-create'),  # Handles GET (list) & POST (create)
     path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),  # Handles GET (retrieve), PUT (update), DELETE (remove)
+    
 ]
+
