2ad07f517f7a7d688892d97c1eadc5e6833df391
Daniel Wavamunno <danl.wavah@gmail.com>
2025-05-09 17:49:30 +0300

    xxx

--- backend/backend/users/urls.py
+++ backend/backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserListCreateView
+from .views import UserListCreateView, UserDetailView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view(), name='user-list-create'),
+    path('', UserListCreateView.as_view(), name='user-list-create'),  # GET (all), POST
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),  # GET (by id), PUT, DELETE
 ]
